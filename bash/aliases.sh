d576c3fd5dc97d306cbaea01f89fd042c293aaf8
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2020-03-02 12:31:39 +0200

    Added tools for updating dotfiles

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Update
-alias update-all="sudo apt update && sudo apt -y upgrade && sudo apt dist-upgrade && sudo apt -y autoremove && snap refresh && upgrade_oh_my_bash"
+alias update-all="sudo apt update && sudo apt -y upgrade && sudo apt dist-upgrade && sudo apt -y autoremove && snap refresh"
