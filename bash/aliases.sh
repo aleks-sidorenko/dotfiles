ce9da832f7a92204a9305d5d26eb71b7cc40ebdd
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2020-05-29 12:23:58 +0300

    Added git branches cleanup alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,3 +2,7 @@
 
 # Update
 alias update-all="sudo apt update && sudo apt -y upgrade && sudo apt dist-upgrade && sudo apt -y autoremove && snap refresh"
+
+
+# git
+alias git-br-delete="git branch | grep -v "master" | xargs git branch -D"
