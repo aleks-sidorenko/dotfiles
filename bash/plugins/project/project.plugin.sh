62bb90ec46e055fbb6578a28cfc8f63e9b4c93c1
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2020-09-24 18:17:49 +0300

    Improved project plugin

--- bash/plugins/project/project.plugin.sh
+++ bash/plugins/project/project.plugin.sh
@@ -4,37 +4,67 @@
 # Project root directory
 export PROJECT_ROOT="${HOME}/Projects"
 export PROJECT_CURRENT="${PROJECT_ROOT}/Current"
+export PROJECT_NAME_FILE="${PROJECT_CURRENT}/.project"
 
+
+project-root() {
+    cd $PROJECT_HOME
+}
+
+project-unset() {
+    _clear_project_name
+    export PROJECT_NAME=
+    export PROJECT_HOME=
+}
+
+_set_project_name() {
+    rm -f $PROJECT_NAME_FILE
+    echo "$1" > $PROJECT_NAME_FILE
+}
+
+_clear_project_name() {
+    _set_project_name ""
+}
 
 # Set current working project context
 project() {
+    
+    export PROJECT_NAME=
+    export PROJECT_HOME=
+
+    # if no arguments
     if [[ -z "${1}" ]]; then
-        if [[ -z "${PROJECT_NAME}" ]]; then
-            echo "Usage: project PROJECT_NAME"
-            return 1
+        if [ -f $PROJECT_NAME_FILE ]; then
+            # read it from file
+            PROJECT_NAME=$(<$PROJECT_NAME_FILE)
+            if [[ -z "${PROJECT_NAME}" ]]; then
+                return 0
+            fi
         else
-            echo "Current project is: [$PROJECT_NAME]"
-            return 0
+            _clear_project_name
         fi
+    else
+        PROJECT_NAME=$1
+        _set_project_name "$PROJECT_NAME"
+    fi
+
+    if [[ ! -z "${PROJECT_NAME}" ]]; then
+        PROJECT_HOME=$PROJECT_CURRENT/$PROJECT_NAME
     fi
     
-    export PROJECT_NAME=$1
-    export PROJECT_HOME=$PROJECT_CURRENT/$PROJECT_NAME
-
     if [[ ! -d "$PROJECT_HOME" ]]
     then
         echo "Project $PROJECT_NAME does not exist in $PROJECT_CURRENT"
         return 1
     fi
 
-    echo "Setting current project to [$PROJECT_NAME]"
-    
-    cd $PROJECT_HOME
-
-    for f in ./.bash/*; do
+    for f in $PROJECT_HOME/.bash/*; do
         source $f
     done
 }
+
+project
+
 
 _project() {
     local projects=("$PROJECT_CURRENT/$2"*)
@@ -42,3 +72,4 @@ _project() {
 }
 
 complete -F _project project
+
